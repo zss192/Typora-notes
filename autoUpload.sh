db1a6765c34ea83c44dd917edf77d41413c3458f
zss192 <zhaoshishun1@gmail.com>
2022-11-17 00:04:37 +0800

    delete notes

--- autoUpload.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-time=$(date "+%Y-%m-%d %H:%M:%S")
-echo "${time}"
-echo "-----------------------------------"
-git add .
-git commit -m "add"
-proxychains git pull origin master
-proxychains git push origin master
